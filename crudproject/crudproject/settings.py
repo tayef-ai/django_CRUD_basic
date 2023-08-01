c32649f119d9b3e3b1673ff2ed0bbb4170fed1b8
Khandaker Tayef Shahriar <tayef12@gmail.com>
2023-08-01 21:27:38 +0600

    Update settings.py

--- crudproject/crudproject/settings.py
+++ crudproject/crudproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y!^eln_5%b$^i0u!3gm8stn$+6!t+sfar$u6x=sk#3^9t!gk-=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
